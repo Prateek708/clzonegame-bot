fd2fc2f7754e1aeb23255e49f0896187db7fbec0
Prateek708 <prateekking340@gmail.com>
2026-05-31 12:23:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,3 +8,17 @@ bot.onText(/\/start/, (msg) => {
 });
 
 console.log("CL Zone Bot Started");
+
+// --- Render Port Error Fix ---
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Bot is running safely!');
+});
+
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
